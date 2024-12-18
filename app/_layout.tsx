aaaa6afb7aee314da8b241f3b65fbf5fd2d6941b
Hoàn <hoan@Bugg.local>
2024-12-18 15:21:27 +0700

    add nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,7 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import "../global.css"
+import React from "react";
 
 export default function RootLayout() {
   return (
